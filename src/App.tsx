4b81ce1a6d61a369df2895b66692d41f9607a9b8
christianjunior05 <bchrist.junior@gmail.com>
2025-09-04 00:32:00 +0000

    ad

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,21 @@ import QuiSommesNous1 from "./pages/QuiSommesNous1";
 import QuiSommesNous from "./pages/QuiSommesNous";
 import QuiSommesNous2 from "./pages/QuiSommesNous2";
 import Parcoursup360 from "./pages/Parcoursup360";
+import Navbar from "./components/Navbar";
+import Contact from "./pages/Contact";
 
 export default function App() {
   return (
     <Router>
       <div className="min-h-screen flex flex-col">
-        
+        <Navbar/>
         <main className="flex-grow">
           <Routes>
             <Route path="/qui-sommes-nous-1" element={<QuiSommesNous1 />} />
             <Route path="/qui-sommes-nous" element={<QuiSommesNous />} />
             <Route path="/qui-sommes-nous-2" element={<QuiSommesNous2 />} />
             <Route path="/parcoursup-360" element={<Parcoursup360 />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
         </main>
         <Footer />
